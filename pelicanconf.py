37da86bd7df5ff9488e5d9978f2072157e145425
Chris Friedline <cfriedline@vcu.edu>
2014-07-27 23:24:51 -0400

    update pagination

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,7 +42,7 @@ SOCIAL = (
 )            
             
             
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
