c1b412632d5ee82709bc3b01fcf9de30fd7ade76
Chris Friedline <cfriedline@vcu.edu>
2015-12-22 12:05:41 -0500

    remove old google url

--- pelicanconf.py
+++ pelicanconf.py
@@ -84,7 +84,6 @@ STATIC_PATHS = ['extra/CNAME',
 
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
-    'extra/google34b2553a47731679.html': {'path': 'google34b2553a47731679.html'},
 }
 
 BOOTSTRAP_THEME = "simplex"
